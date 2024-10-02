82802217d6747c2d538bdc551a5aa089dc1a90a3
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-10-02 13:13:32 +0300

    Cleaning up comment

--- app/src/utils/apolloClient.js
+++ app/src/utils/apolloClient.js
@@ -4,7 +4,6 @@ import Constants from 'expo-constants';
 const createApolloClient = () => {
   console.log(`https://api.digitransit.fi/routing/v1/routers/finland/index/graphql?${Constants.expoConfig.extra.api_name}=${Constants.expoConfig.extra.api_key}`)
   return new ApolloClient({
-    //uri: 'https://api.digitransit.fi/routing/v1/routers/finland/index/graphql?digitransit-subscription-key=c6dedbf8581e4986b5e21fbedd93e730',
     uri: `https://api.digitransit.fi/routing/v1/routers/finland/index/graphql?${Constants.expoConfig.extra.api_name}=${Constants.expoConfig.extra.api_key}`,
     cache: new InMemoryCache(),
   });
