fcb7a9029e9c49ed60720061b77ff741fcca89c5
Juska Kellokumpu <juskakaro.kellokumpu@tuni.fi>
2024-10-02 18:26:38 +0300

    Expo web

--- app/src/utils/apolloClient.js
+++ app/src/utils/apolloClient.js
@@ -2,7 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import Constants from 'expo-constants';
 
 const createApolloClient = () => {
-  console.log(`https://api.digitransit.fi/routing/v1/routers/finland/index/graphql?${Constants.expoConfig.extra.api_name}=${Constants.expoConfig.extra.api_key}`)
   return new ApolloClient({
     uri: `https://api.digitransit.fi/routing/v1/routers/finland/index/graphql?${Constants.expoConfig.extra.api_name}=${Constants.expoConfig.extra.api_key}`,
     cache: new InMemoryCache(),
