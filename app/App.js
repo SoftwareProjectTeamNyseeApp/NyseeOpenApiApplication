e96a471bc36b3689d1b30f9866a1dbff4056f2a5
Long Le <long.le21062004@gmail.com>
2024-10-21 13:29:06 +0300

    VehicleInfo demo

--- app/App.js
+++ app/App.js
@@ -7,18 +7,20 @@ import DestinationSelect from './src/components/DestinationSelect';
 import createApolloClient from './src/utils/apolloClient';
 import Constants from 'expo-constants';
 import TestQuery from './src/components/TestQuery';
+import VehicleInfo from './src/components/VehicleInfo';
 
 const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <ApolloProvider client={apolloClient}>
-     <View style={styles.container}>
-        <Text>Hello Nysee!</Text>
+      <View style={styles.container}>
+        <Text>Nysee Application</Text>
         <StatusBar style="auto" />
         <DestinationSelect />
         {/* <Test /> */}
         {/* <TestQuery /> */}
+
       </View>
     </ApolloProvider>
   );
